18ddaa1e4abcfa37e099def1923d1e0df535c659
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-31 02:04:18 +0400

    feat(Input): Added Input component and change style

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import './Input.css';
+import cn from 'classnames';
+import { INPUT_TYPES } from '../../helpers/constants';
+export function Input({ onChange, value, placeholder, name }) {
+  return (
+    <input
+      className={cn(
+        { mainInput: name === INPUT_TYPES.mainInput },
+        { todoListInput: name === INPUT_TYPES.todoInput }
+      )}
+      type="text"
+      name={name}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+      autoFocus={true}
+    />
+  );
+}
+
+Input.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  name: PropTypes.string.isRequired,
+};
