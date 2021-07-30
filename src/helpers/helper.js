fab17a24dc56fb1b10eecf34b97902a59e71b4e7
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-31 02:11:30 +0400

    feat(helper): Added localStorage

---
+++ src/helpers/helper.js
@@ -0,0 +1,12 @@
+export function setLocalStorage(state) {
+  localStorage.setItem('_state', JSON.stringify(state));
+}
+export function getLocalStorage() {
+  return JSON.parse(localStorage.getItem('_state'));
+}
+
+const createNewId = () => {
+  return Date.now();
+};
+
+export { createNewId };
