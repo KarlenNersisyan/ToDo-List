f9a5e36cbb88eacd215ced62ca3b54792388feff
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-31 02:09:29 +0400

    feat(Button): Added Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+export function Button({ isDisable, handleClick, text, className }) {
+  return (
+    <button className={className} disabled={isDisable} onClick={handleClick}>
+      {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  isDisable: PropTypes.bool,
+  handleClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
